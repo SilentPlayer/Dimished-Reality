6773c4015aa36a55d752185b362eeafa4fcdcc42
erwin andre <erwin@erwins-macbook-pro.local>
2018-12-06 01:38:29 +0100

    Initial Commit

---
+++ Viewer/ViewController.h
@@ -0,0 +1,15 @@
+/*
+  This file is part of the Structure SDK.
+  Copyright © 2016 Occipital, Inc. All rights reserved.
+  http://structure.io
+*/
+
+#import <UIKit/UIKit.h>
+#define HAS_LIBCXX
+#import <Structure/Structure.h>
+
+@interface ViewController : UIViewController <STSensorControllerDelegate>
+
++ (instancetype) viewController;
+
+@end
